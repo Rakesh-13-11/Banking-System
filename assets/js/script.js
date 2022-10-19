4207c2a0e5485efeb52e3e74072924d4e2a246e4
Rakesh Kumar Meena <rakeshkumarthanwal@gmail.com>
2022-10-19 22:23:17 +0530

    Deploy

---
+++ assets/js/script.js
@@ -0,0 +1,28 @@
+
+function sendMoney(){
+   let myAccountBalanceAmount = document.getElementById("myAccountBalance").innerText*1;
+   var enterName = document.getElementById("enterName").value;
+   var enterAmount = (document.getElementById("enterAmount").value)*1;
+   
+   if ((myAccountBalanceAmount - enterAmount) < 0) {
+      alert("Insufficient Balance.")
+   } else {
+      var findUserBankAccount = enterName + "BankBalance";
+      var finalAmount = parseInt(document.getElementById(findUserBankAccount).innerHTML) + enterAmount;
+      var myAccountBalance = parseInt(document.getElementById("myAccountBalance").innerText) - enterAmount
+      document.getElementById("myAccountBalance").innerText = myAccountBalance
+      document.getElementById(findUserBankAccount).innerHTML = finalAmount;
+      alert(`Successful Transaction !!  
+      $${enterAmount} is sent to ${enterName}@email.com.`)
+
+      // transaction history 
+      var createPTag = document.createElement("li");
+      var textNode = document.createTextNode(`$${enterAmount} is sent to recepient with Email-id ${enterName}@email.com on ${Date()}.`);
+      createPTag.appendChild(textNode);
+      var element = document.getElementById("transaction-history-body");
+      element.insertBefore(createPTag, element.firstChild);
+   }
+}
+Footer
+
+
